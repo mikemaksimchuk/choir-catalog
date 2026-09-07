65e7e3e85d72b4302cc447fe9dff88060630937f
mikemaksimchuk <mike@maksimch.uk>
2026-09-07 14:40:11 -0400

    modify all

--- scripts/normalize-catalog.mjs
+++
@@ -1,8 +0,0 @@
-import fs from 'node:fs/promises'
-import { parseCSV, serializeCSV, sortCatalog } from '../shared/catalog.js'
-
-const catalogPath = new URL('../public/data.csv', import.meta.url)
-const contents = await fs.readFile(catalogPath, 'utf8')
-const entries = sortCatalog(parseCSV(contents))
-await fs.writeFile(catalogPath, serializeCSV(entries), 'utf8')
-console.log(`Normalized ${entries.length} catalog entries.`)
